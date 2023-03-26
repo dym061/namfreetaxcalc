3502f1b48dc657769a089127a4a20ccb6ea0b6e0
dym061 <dominik.vonfrancois@hotmail.com>
2023-03-26 13:46:28 +0200

    google analytics updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import '@/styles/style.css'
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
