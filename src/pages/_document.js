4c0b94ae228ffe308b91e2c0a7ff79410d4b7d98
Dimi <102525636+dym061@users.noreply.github.com>
2026-02-06 00:33:49 +0200

    Remove PicoCSS and refresh base styling

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
